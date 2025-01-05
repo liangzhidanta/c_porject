e026bade44bee2d9dea6091134d9ad86a0defb43
liangzhidanta_wsh <1652168492@qq.com>
2025-01-05 21:23:04 +0800

    feat(1.5): 添加 r+ 和 rb+ 文件读写模式的示例代码
    - 新建 file.txt 和 main.c 文件
    - 实现了使用 r+ 和 rb+ 模式打开文件进行读写操作的示例
    - 展示了文本模式和二进制模式下文件读写的不同行为

---
+++ 1.5/r+与rb+的区别/main.c
@@ -0,0 +1,31 @@
+/*
+ * @Author: liangzhidanta_wsh 1652168492@qq.com
+ * @Date: 2025-01-05 20:41:54
+ * @LastEditors: liangzhidanta_wsh 1652168492@qq.com
+ * @LastEditTime: 2025-01-05 21:23:03
+ * @FilePath: \c_porject\1.5\r+与rb+的区别\main.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//r+  文本模式下，写入\n，存储的是\r\n，读取时，\r\n会变为\n
+//rb+ 二进制模式下，写入\n，存储的是\n，读取时，\r\n会变为\n
+int main(int argc, char *argv[])
+{   
+    FILE *fp;
+    fp = fopen("file.txt", "r+");
+    if (fp == NULL) {
+        perror("fopen");
+        return -1;
+    }
+    int ret = fwrite("hello world",sizeof(char),11,fp);
+    printf("ret = %d\n",ret);//fwrite成功，返回值代表写入的字符数
+    fseek(fp,0,SEEK_SET);//光标回到文件开头
+    char c[100] = {0};
+    ret = fread(c,sizeof(char),100,fp);
+    printf("ret = %d\n",ret);
+    printf("%s\n",c);
+    fclose(fp);
+    return 0;
+}
